95af4d3ab58c1bf5a5c1b7430ed978d4276738ee
sandesh pandey <sandesh.pandey00112@gmail.com>
2025-07-04 12:11:41 +0545

    setting file error handled succesfully and bootstrap applied too

--- main_app/settings.py
+++ main_app/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'main_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'main_app', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR, "static")],
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')  # ✅ this is correct
+]
 
 #for image handling
 MEDIA_URL = '/media/'
