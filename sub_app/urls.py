3f4d4ccd151701e3abdc822b03cd7b269cd8755f
sandesh pandey <sandesh.pandey00112@gmail.com>
2025-07-04 20:03:07 +0545

    View error occure

--- sub_app/urls.py
+++ sub_app/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 from . import views 
 
 
+
+
 urlpatterns = [
-path('',views.index,name='index')
-   
+    path('', views.index, name='list_tweet'),  # Changed from 'listtweet' to 'list_tweet'
+    
 ]
+   
+
+"""path('', views.list_tweet, name='list_tweet'),  # Changed from 'listtweet' to 'list_tweet'
+    path('create/', views.create_tweet, name='create_tweet'),  # Changed from 'tweet_create' to 'create_tweet'
+    path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
+    path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+]"""
